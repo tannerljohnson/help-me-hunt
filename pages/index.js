2ba3f4997b6d53cd8259a70d33e1d2fbef8e097b
Tanner Johnson <tanner.johnson@gusto.com>
2020-12-05 17:37:18 -0700

    send query params

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,67 @@
 import React, { useState } from "react";
 import Layout from '../components/Layout.js'
-import { Button, ListGroup, ListGroupItem, Col, Row, CardBody, CardTitle, CardSubtitle, Card } from "shards-react";
+import { Button, FormSelect, Form, FormGroup, CardBody, CardTitle, CardSubtitle, Card } from "shards-react";
 
 export const BASE_ENDPOINT = 'https://cqiqnxsapf.execute-api.us-west-2.amazonaws.com/Prod';
-
-const fetchSeasons = async () => {
-  const res = await fetch(`${BASE_ENDPOINT}/seasons`);
-  return await res.json();
+export const DEFAULT_STATE = 'Idaho';
+export const capitalize = s => {
+  if (typeof s !== 'string') return '';
+  return s.charAt(0).toUpperCase() + s.slice(1);
 };
 
 const Home = () => {
   const [searchResults, setSearchResults] = useState([]);
+  const [stateSelection, setStateSelection] = useState(DEFAULT_STATE);
+  const [speciesSelection, setSpeciesSelection] = useState(DEFAULT_STATE);
+  const [loading, setLoading] = useState(false);
+
+  const fetchSeasons = async () => {
+    setLoading(true);
+    const queryParams = `state=${stateSelection}&species=${speciesSelection}`;
+    const res = await fetch(`${BASE_ENDPOINT}/seasons?${queryParams}`);
+    const data = await res.json();
+    setLoading(false);
+    return data;
+  };
+
   return (
     <Layout>
       <h1>
         Help me hunt
       </h1>
-      <Button onClick={async () => {
+      <Form>
+        <FormGroup>
+          <label htmlFor="#state">Select state</label>
+          <FormSelect id="#state" onChange={e => setStateSelection(e.target.value)}>
+            <option value="Idaho">Idaho</option>
+          </FormSelect>
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="#species">Select game</label>
+          <FormSelect id="#species" onChange={e => setSpeciesSelection(e.target.value)}>
+            <option value="deer">Deer</option>
+          </FormSelect>
+        </FormGroup>
+      </Form>
+      <Button disabled={loading} onClick={async () => {
         const results = await fetchSeasons();
         setSearchResults(results);
       }}>
-        Show all seasons
+        {loading ? (
+          <>
+            Loading...
+          </>
+        ) : (
+          <>Find seasons</>
+        )}
       </Button>
       <div>
       {!!searchResults.length && (
-        searchResults.map(result => (
+        searchResults.map((result, i) => (
             <Card key={result.id} style={{ marginTop: '15px' }}>
               <CardBody>
-                <CardTitle>{result.species}</CardTitle>
-                <CardSubtitle>{result.subspecies ?? 'any'}</CardSubtitle>
+                <CardTitle>{i+1}. {capitalize(result.species)}</CardTitle>
+                <CardSubtitle>Allowed types: {result.subspecies ? capitalize(result.subspecies) : 'Any'}</CardSubtitle>
                 <div>
                   Season: {result.seasonStart} - {result.seasonEnd}
                 </div>
